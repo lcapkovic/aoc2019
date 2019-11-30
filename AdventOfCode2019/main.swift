7819d533f494d02f5dfbf3adb7d68cc4f9b06048
Lukas Capkovic <lcapkovic@gmail.com>
2019-11-30 15:35:54 +0100

    Initial Commit

---
+++ AdventOfCode2019/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventOfCode2019
+//
+//  Created by Lukas Capkovic on 30/11/2019.
+//  Copyright © 2019 Lukas Capkovic. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
